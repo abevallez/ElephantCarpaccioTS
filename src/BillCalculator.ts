111237671987c2915b292207ac83f510f5f4f6b0
Abraham Vallez <abevallez@gmail.com>
2021-12-20 19:55:04 +0100

    second test with UT tax applied

--- src/BillCalculator.ts
+++ src/BillCalculator.ts
@@ -1,5 +1,6 @@
 export class BillCalculator {
     public calculateBill(numProducts: number, priceByProduct: number): number {
-        return 100
+        const ut_tax = 0.0685
+        return 100 * ut_tax
     }
 }
--- tests/BillCalculator.test.ts
+++ tests/BillCalculator.test.ts
@@ -1,9 +1,11 @@
 import { BillCalculator } from '../src/BillCalculator'
 
 describe('tests BillCalculator', () => {
-    test ('total is the price of a product', () => {
-        let billCalculator: BillCalculator= new BillCalculator()
+
+    test ('tax UT is apply to total for a product', () => {
+        const ut_tax = 0.0685
+        let billCalculator: BillCalculator = new BillCalculator()
         let totalBill: number = billCalculator.calculateBill(1,100)
-        expect(totalBill).toBe(100)
+        expect(totalBill).toBe(100 * ut_tax)
     });
 })
