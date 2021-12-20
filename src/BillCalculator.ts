aff39545d26b537a74bc25f62375c87659578009
Abraham Vallez <abevallez@gmail.com>
2021-12-20 20:03:07 +0100

    added differente prices for 1 product test

--- src/BillCalculator.ts
+++ src/BillCalculator.ts
@@ -1,6 +1,6 @@
 export class BillCalculator {
     public calculateBill(numProducts: number, priceByProduct: number): number {
         const ut_tax = 0.0685
-        return 100 * ut_tax
+        return priceByProduct * ut_tax
     }
 }
--- tests/BillCalculator.test.ts
+++ tests/BillCalculator.test.ts
@@ -2,10 +2,15 @@ import { BillCalculator } from '../src/BillCalculator'
 
 describe('tests BillCalculator', () => {
 
-    test ('tax UT is apply to total for a product', () => {
+    test.each([
+        [100],
+        [50],
+        [40],
+        [1456]
+    ]) ('tax UT is apply to total for a product', (price) => {
         const ut_tax = 0.0685
         let billCalculator: BillCalculator = new BillCalculator()
-        let totalBill: number = billCalculator.calculateBill(1,100)
-        expect(totalBill).toBe(100 * ut_tax)
+        let totalBill: number = billCalculator.calculateBill(1, price)
+        expect(totalBill).toBe(price * ut_tax)
     });
 })
