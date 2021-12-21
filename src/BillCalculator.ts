d96c0922d4984a4c11422f45ce856694362342a6
Abraham Vallez <abevallez@gmail.com>
2021-12-21 13:17:27 +0100

    refactor products as array of prices

--- src/BillCalculator.ts
+++ src/BillCalculator.ts
@@ -1,7 +1,7 @@
 export class BillCalculator {
-    public calculateBill(numProducts: number, priceByProduct: number, estate: string): number {
+    public calculateBill(numProducts: number, prices: number[], estate: string): number {
         const ut_tax = 0.0685
-        let total = priceByProduct * numProducts
+        let total = prices.reduce((a, b) => a + b, 0)
         if (total > 1000)
             total = total * 0.03
         return total * ut_tax
--- tests/BillCalculator.test.ts
+++ tests/BillCalculator.test.ts
@@ -2,44 +2,59 @@ import { BillCalculator } from '../src/BillCalculator'
 
 describe('tests BillCalculator', () => {
 
+    let billCalculator: BillCalculator
+    const ut_tax = 0.0685
+
+    beforeEach(() => {
+        billCalculator = new BillCalculator()
+    })
+
     test.each([
-        [100],
-        [50],
-        [40],
-    ]) ('tax UT is applied to total for 1 product < 1000$', (price) => {
-        const ut_tax = 0.0685
-        let billCalculator: BillCalculator = new BillCalculator()
-        let totalBill: number = billCalculator.calculateBill(1, price, 'UT')
-        expect(totalBill).toBe(price * ut_tax)
+        [[100]],
+        [[50]],
+        [[40]],
+    ]) ('tax UT is applied to total for 1 product < 1000$', (prices) => {
+        let totalBill: number = billCalculator.calculateBill(prices.length, prices, 'UT')
+        expect(totalBill).toBe(sumPrices(prices) * ut_tax)
     });
     
     test('discount 3% and UT tax is applied when total > 1000$',() => {
-        const ut_tax = 0.0685
-        const price = 1456
-        let billCalculator: BillCalculator = new BillCalculator()
-        let totalBill: number = billCalculator.calculateBill(1, price, 'UT')
-        let totalExpected: number = (price * 0.03) * ut_tax
+        let prices: number[] = [1456]
+        const discount = 0.03
+
+        let totalBill: number = billCalculator.calculateBill(prices.length, prices, 'UT')
+        let totalExpected: number = (sumPrices(prices) * discount) * ut_tax
+
         expect(totalBill).toBe(totalExpected)
     });
 
     test('calculate total with many products, same price by product < 1000$', () => {
-        let billCalculator: BillCalculator = new BillCalculator()
-        let numProducts: number = 2
-        let price: number = 50
-        const ut_tax = 0.0685
-        let totalBill: number = billCalculator.calculateBill(numProducts, price, 'UT')
-        let totalExpected: number = (price * numProducts) * ut_tax
+        let prices: number[] = [50, 50]
+
+        let totalBill: number = billCalculator.calculateBill(prices.length, prices, 'UT')
+        let totalExpected: number = sumPrices(prices) * ut_tax
+
         expect(totalBill).toBe(totalExpected)
     });
 
     test('calculate total with many products, same price by product > 1000$', () => {
-        let billCalculator: BillCalculator = new BillCalculator()
-        let numProducts: number = 2
-        let price: number = 600
-        const ut_tax = 0.0685
+        let prices: number[] = [600, 600]
         const discount = 0.03
-        let totalBill: number = billCalculator.calculateBill(numProducts, price, 'UT')
-        let totalExpected: number = ((price * numProducts) * discount) * ut_tax
+
+        let totalBill: number = billCalculator.calculateBill(prices.length, prices, 'UT')
+        let totalExpected: number = (sumPrices(prices) * discount) * ut_tax
+        expect(totalBill).toBe(totalExpected)
+    });
+
+    test('calculate total with many products, differentes prices by product < 1000$', () => {
+        let prices: number[] = [300, 50]
+        const discount = 0.03
+        let totalBill: number = billCalculator.calculateBill(prices.length, prices, 'UT')
+        let totalExpected: number = sumPrices(prices) * ut_tax
         expect(totalBill).toBe(totalExpected)
     });
 })
+
+function sumPrices(prices: number[]) {
+    return prices.reduce((a, b) => a + b, 0);
+}
